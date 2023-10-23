6503a065b240a6c3b6eeb26e5bad47fc334398af
kuriatsu <kuriatsubayashi712@gmail.com>
2023-10-23 19:41:13 +0900

    add tsubasa

---
+++ keyboards/tsubasa/config.h
@@ -0,0 +1,25 @@
+// Copyright 2023 kuriatsu (@kuriatsu)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//
+
+#define ADNS5050_SCLK_PIN GP14
+#define ADNS5050_SDIO_PIN GP15
+#define ADNS5050_CS_PIN GP26
